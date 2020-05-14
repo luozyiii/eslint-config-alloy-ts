98aafdb679753cb6db09f537da10b012ad40bc39
luozyiii <924361501@qq.com>
2020-05-14 22:57:05 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+// .eslintrc.js
+// extends 这一句很关键 plugin:prettier/recommended
+module.exports = {
+  extends: ["alloy", "alloy/typescript", "plugin:prettier/recommended"],
+  env: {
+    // 你的环境变量（包含多个预定义的全局变量）
+    //
+    // browser: true,
+    // node: true,
+    // mocha: true,
+    // jest: true,
+    // jquery: true
+  },
+  globals: {
+    // 你的全局变量（设置为 false 表示它不允许被重新赋值）
+    //
+    // myGlobal: false
+  },
+  rules: {
+    // 自定义你的规则
+  },
+};
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+console.log("hello eslint");
+let a = 1;
+if (a === 1) {
+  return;
+}
